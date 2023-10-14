94db106105a060e5c9eee94a28309545fdae2fae
Bailey Mejia <baileymejia@Baileys-MBP.lan>
2023-10-13 22:36:53 -0700

    Created the about me section

---
+++ src/Components/About/About.styled.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+
+export const AboutContainer = styled.section`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+align-items: center;
+text-align: center;
+padding-top: 30px;
+`;
+
+export const Meimage = styled.img`
+border-radius: 50%;
+overflow: hidden;
+width: 250px;
+height: 250px;
+object-fit: cover;
+`;
+
+export const Title = styled.h2`
+color: black;
+font-weight: bolder;
+`;
+
+export const AboutBody = styled.p`
+width: 80%;
+line-height: 25px;
+`;
+
+
+
